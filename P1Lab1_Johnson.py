100024388ca3bb6f903f0be1c6d0d4f991672285
johnsond2483 <johnsond2483@student.faytechcc.edu>
2026-09-01 18:48:19 +0000

    P1Lab1 Done

---
+++ P1Lab1_Johnson.py
@@ -0,0 +1,19 @@
+# CTI 110
+# P1LAB1
+# Darion, J
+# 9/1/26
+# Our First Python Program
+
+# First, we're going to greet the user
+print("hello")
+
+# Next, ask their first name
+#print("What's your first name?")
+first_name = input("What is your first name? ")
+
+#Then, do the same for the last name
+Last_name = input("Great! Now what's your last name? ")
+
+# Then say back the name
+print("It it very nice to meet you", first_name, Last_name, ":)")
+
