7e65984e1ab361f66bb8f8da431ed3cdedf3a81d
johnsond2483 <johnsond2483@student.faytechcc.edu>
2026-09-15 18:30:35 +0000

    P2Lab2

---
+++ P2Lab2_JohnsonDarion.py
@@ -0,0 +1,25 @@
+# CTI-110
+# P2Lab2
+# Darion J
+# 9/15/26
+
+# Car dictionary that lets us look up mpg
+# From that mpg, find gallons burned per X miles.
+
+cars ={
+    "Camaro": 18.21,
+    "Prius" : 52.36,
+    "Model S" : 110,
+    "Silverado" : 26, 
+}
+print(cars)
+car_keys = cars.keys()
+print(car_keys)
+# Choose the car
+car = input ("Enter a vehicle to see its mpg: ")
+mpg = cars[car]
+print(f"The MPG of {car} is {mpg} miles per gallon.")
+# Ask user for miles, output gas used
+miles= float(input("How many miles will you drive? "))
+gallons_used = miles / mpg # miles divided by miles per gallon gives units in gallons
+print(f"Driving{car} for {miles} miles will used {gallons_used:.2f} gallons of gas.")
